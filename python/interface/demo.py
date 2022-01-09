d9b2f4a842d78c48becd43cf8536c617d84f015d
zhoua0805 <alicezhou0805@gmail.com>
2022-01-09 07:58:45 -0500

    add visualization to demo

--- python/interface/demo.py
+++ python/interface/demo.py
@@ -1,15 +1,23 @@
 from PerfectSalesman.python.interface.utils import discount_range
 from PerfectSalesman.python.interface.utils import find_best_profit_in_range
+from PerfectSalesman.python.interface.utils import plotProductTrend
 from model.Model import Model
+from datetime import datetime
+import torch
+import pandas as pd
 
 def demo():
-    # TODO: get current month and decide path
-    modelPath = '/content/december.pt'
-    model = Model()
-    model.load_state_dict(torch.load(modelPath, map_location=torch.device('cpu')))
     products = ["Accessories", "Appliances", "Art", "Binders", "Bookcases", "Chairs", 
               "Copiers", "Envelopes", "Fasteners", "Furnishings", "Labels", "Machines", 
               "Paper", "Phones", "Storage", "Supplies", "Tables"]
+    months = ['january', 'february', 'march', 'april', 'may', 'june', 
+          'july', 'august', 'september', 'october', 'november', 'december' ]
+    currentMonth = months[datetime.now().month-1]
+    # TODO: decide path
+    # modelPath = f'/content/{currentMonth}.pt'
+    modelPath = f'/content/december.pt'
+    model = Model()
+    model.load_state_dict(torch.load(modelPath, map_location=torch.device('cpu')))
 
     while (True):
         mode = input("\nWelcome to PerfectSalesman!\nDo you want to find the best product to go on sale for this month (a),\nor input a custom data point (b),\nor exit (q)?\n")
@@ -17,10 +25,12 @@ def demo():
             print("Thank you for participating!")
             break
         elif (mode == "a"):
-            discountIncrement = 10
+            discountIncrement = 5
             result = find_best_profit_in_range(model, products, discount_range(discountIncrement))
-            print(f"The best product to go in sale is: {result[0]} for {result[1]}%.")
+            print("-"*70)
+            print(f"The best product to go in sale in {currentMonth} is: {result[0]} for {result[1]}%.")
             print(f"You will gain ${result[2][2]:.2f} in profit with {int(result[2][1])} Qty sold.")
+            print("-"*70)
         elif (mode == "b"):
             # User specified discount
             product = input("Please enter a product category: ")
@@ -31,8 +41,11 @@ def demo():
                 print("Input Error, Please Try Again.")
             else:
                 result = model(torch.Tensor([discount]+pd.get_dummies(pd.Series(products))[product].tolist()))
+                print("-"*70)
                 print(f"You will gain ${result[2]:.2f} in profit with {int(result[1])} Qty sold.")
-                #TODO: visualize graph
+                print("-"*70)
+                plotProductTrend(model, product, [discount]+result.tolist
+                                 (), products, discount_range(2))
         else:
             print("Input Error, Please Try Again.")
 
--- python/interface/utils.py
+++ python/interface/utils.py
@@ -1,4 +1,5 @@
 import torch
+import matplotlib.pyplot as pyplot
 
 def discount_range(increment):
     discount = [i for i in range(0, 100, increment)]
@@ -18,3 +19,26 @@ def find_best_profit_in_range(model, products, discount_range):
     discount_profit.sort(key=(lambda x: x[2][profit_index]), reverse=True)
 
     return discount_profit[0]
+
+def plotProductTrend(model, product, result, products, discount_range):
+    discount_sales = [model(torch.Tensor([d]+pd.get_dummies(pd.Series(products))[product].tolist()))[0] for d in discount_range]
+    discount_qty = [model(torch.Tensor([d]+pd.get_dummies(pd.Series(products))[product].tolist()))[1] for d in discount_range]
+    discount_profit = [model(torch.Tensor([d]+pd.get_dummies(pd.Series(products))[product].tolist()))[2] for d in discount_range]
+
+    pyplot.plot(discount_range, discount_sales, label='Sales')
+    pyplot.plot(result[0], result[1], 'r*')
+    pyplot.plot(discount_range, discount_profit, label='Profit')
+    pyplot.plot(result[0], result[3], 'r*', label="You are here")
+    pyplot.legend()
+    pyplot.ylabel('Dollar')
+    pyplot.xlabel('% of discount')
+    pyplot.title(f'Sales & Profit vs. percentage discount for {product}')
+    pyplot.show()
+    pyplot.plot(discount_range, discount_qty, label='Qty')
+    pyplot.plot(result[0], result[2], 'r*', label="You are here")
+    pyplot.legend()
+    pyplot.ylabel('Qty')
+    pyplot.xlabel('% of discount')
+    pyplot.title(f'Qty vs. percentage discount for {product}')
+    pyplot.show()
+    return
