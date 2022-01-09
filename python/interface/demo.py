7b59c102c3e1e18398b1b51e61c118107c367fd2
zhoua0805 <alicezhou0805@gmail.com>
2022-01-09 00:36:54 -0500

    finish inteface

--- python/interface/demo.py
+++ python/interface/demo.py
@@ -1,10 +1,19 @@
 from PerfectSalesman.python.interface.utils import discount_range
-
+from PerfectSalesman.python.interface.utils import find_best_profit_in_range
+from model.Model import Model
 
 def select_mode(mode="Best"):
+    # TODO: get current month and decide path
+    modelPath = '/content/december.pt'
+    model = Model()
+    model.load_state_dict(torch.load(modelPath, map_location=torch.device('cpu')))
+
     if mode == "Best":
-        # TODO: do a loop for all discounts in the range
-        pass
+        discountIncrement = 20
+        products = ["Accessories", "Appliances", "Art", "Binders", "Bookcases", "Chairs", 
+              "Copiers", "Envelopes", "Fasteners", "Furnishings", "Labels", "Machines", 
+              "Paper", "Phones", "Storage", "Supplies", "Tables"]
+        find_best_profit_in_range(model, products, discount_range(discountIncrement))
     else:
         # ? User specified discount
         discount = input("Please enter discount percentage (format: dd%)")
--- python/interface/utils.py
+++ python/interface/utils.py
@@ -1,13 +1,20 @@
+import torch
+
 def discount_range(increment):
     discount = [i for i in range(0, 100, increment)]
     return discount
 
 
 # Returns tuple of format (discount, profit)
-def find_best_profit_in_range(model, discount_range):
-    profit_index = 0
+def find_best_profit_in_range(model, products, discount_range):
+    profit_index = 2
 
-    discount_profit = [(i, model(i)[profit_index]) for i in discount_range]
-    discount_profit.sort(key=(lambda x: x[1]), reverse=True)
+    all_combinations = []
+    for i in products: 
+        for j in discount_range:
+            all_combinations.append((i,j))
+
+    discount_profit = [(p, d, model(torch.Tensor([d]+pd.get_dummies(pd.Series(products))[p].tolist()))) for (p, d) in all_combinations]
+    discount_profit.sort(key=(lambda x: x[2][profit_index]), reverse=True)
 
     return discount_profit[0]
